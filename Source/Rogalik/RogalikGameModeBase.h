e7fabf74cc6b2b3e18e113b4251eb974731c17ba
Kamil Waligóra <k.waligora37@gmail.com>
2022-01-31 07:56:08 +0100

    Setup UE project

---
+++ Source/Rogalik/RogalikGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RogalikGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGALIK_API ARogalikGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
